bce2f16a3221e685064fbe7155c30d638cbc8380
DuyStar2k3 <nguyenduy13572468@gmail.com>
2023-12-16 17:16:49 +0700

    toi ưu thiết bị

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -17,24 +17,15 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         return android;
       case TargetPlatform.iOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for ios - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
+        return ios;
       case TargetPlatform.macOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for macos - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
+        return macos;
       case TargetPlatform.windows:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for windows - '
@@ -52,11 +43,39 @@ class DefaultFirebaseOptions {
     }
   }
 
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyDrx5DKQ-p_UkbV3GwJ_gnMkBWrEBSUFo8',
+    appId: '1:264773014211:web:c4607b1017009a0ae50876',
+    messagingSenderId: '264773014211',
+    projectId: 'photo-share-79468',
+    authDomain: 'photo-share-79468.firebaseapp.com',
+    storageBucket: 'photo-share-79468.appspot.com',
+    measurementId: 'G-6HSJRM63FY',
+  );
+
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyDs2L-2vVfP_ezN_4oTdmSaigf9Hi8S_80',
     appId: '1:264773014211:android:197f927b14d69992e50876',
     messagingSenderId: '264773014211',
     projectId: 'photo-share-79468',
     storageBucket: 'photo-share-79468.appspot.com',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'AIzaSyAng8TXq7Ye6SAddEVgAyOGDeMTRSgPUkw',
+    appId: '1:264773014211:ios:6f8ac3659cef7e4de50876',
+    messagingSenderId: '264773014211',
+    projectId: 'photo-share-79468',
+    storageBucket: 'photo-share-79468.appspot.com',
+    iosBundleId: 'com.example.devUploadImage',
+  );
+
+  static const FirebaseOptions macos = FirebaseOptions(
+    apiKey: 'AIzaSyAng8TXq7Ye6SAddEVgAyOGDeMTRSgPUkw',
+    appId: '1:264773014211:ios:f92aa2cd408559dfe50876',
+    messagingSenderId: '264773014211',
+    projectId: 'photo-share-79468',
+    storageBucket: 'photo-share-79468.appspot.com',
+    iosBundleId: 'com.example.devUploadImage.RunnerTests',
   );
 }
